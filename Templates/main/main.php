268997277c6275bf7b1a1b09fea6bde1be682dba
OLEG MANDRYCHENKO <mandrik89@gmail.com>
2020-04-24 13:06:29 +0200

    update

--- Templates/main/main.php
+++ Templates/main/main.php
@@ -1,39 +1,7 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>My blog</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<table class="layout">
-    <tr>
-        <td colspan="2" class="header">
-            My Blog
-        </td>
-    </tr>
-    <tr>
-        <td>
-            <?php foreach ($articles as $article): ?>
-                <h2><?= $article['name'] ?></h2>
-                <p><?= $article['text'] ?></p>
-                <hr>
-            <?php endforeach; ?>
-        </td>
-
-        <td width="300px" class="sidebar">
-            <div class="sidebarHeader">Menu</div>
-            <ul>
-                <li><a href="/">Main Page</a></li>
-                <li><a href="/about-me">About</a></li>
-            </ul>
-        </td>
-    </tr>
-    <tr>
-        <td class="footer" colspan="2">All rights reserved 2020</td>
-    </tr>
-</table>
-
-</body>
-</html>
+<?php include __DIR__ . '/../header.php'; ?>
+<?php foreach ($articles as $article): ?>
+    <h2><?= $article['name'] ?></h2>
+    <p><?= $article['text'] ?></p>
+    <hr>
+<?php endforeach; ?>
+<?php include __DIR__ . '/../footer.php'; ?>
--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -2,21 +2,26 @@
 
 namespace Controllers;
 
+use View\View;
+use Services\Db;
+
 class MainController
 {
-    public function main()
+    /** @var View */
+    private $view;
+
+    /** @var Db */
+    private $db;
+
+    public function __construct()
     {
-        
-        $articles = [
-            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
-        ];
-        
-        include __DIR__ . '/../../templates/main/main.php';
+        $this->view = new View(__DIR__ . '/../../templates');
+        $this->db = new Db();
     }
 
-    public function sayHello(string $name) 
+    public function main()
     {
-        echo 'Привет, ' . $name;
+        $articles = $this->db->query('SELECT * FROM `articles`;');
+        $this->view->renderHtml('main/main.php', ['articles' => $articles]);
     }
-}
+}   
